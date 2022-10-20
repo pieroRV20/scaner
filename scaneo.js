1607765072b07044be19088a92a1e1c771cd403e
pieroRV20 <101838439+pieroRV20@users.noreply.github.com>
2022-10-19 20:35:27 -0500

    Update scaneo.js

--- scaneo.js
+++ scaneo.js
@@ -1,34 +1,24 @@
-const response = document.getElementById("response");
-const img_true = document.getElementById("img_true");
-const btnScanner = document.getElementById("btnScanner");
-let scanner = new Instascan.Scanner({
-        continuous: true,
-        video: document.getElementById('preview'),
-        mirror: false,
-        refractoryPeriod: 5000,
-        scanPeriod: 5
-    } );
-scanner.addListener('scan', function(content) {
-    img_true.classList.remove("block")
-    img_true.classList.add("none")
+scanner.addListener('scan', function(content) {   
     fetch('https://scanner-f97e1-default-rtdb.firebaseio.com/clientes.json')
     .then((response) => response.json())
     .then((data) => {
         data.map((cliente,index)=>{
             let nombre=content.split("-");
+            img_true.classList.remove("block")
+            img_true.classList.add("none")
             response.classList.remove("none")
             response.classList.add("block")
             if(cliente.content===content ){
                 if (cliente.number_scanners==0) {
                     response.innerHTML=nombre[0]+" fue verificado "
-                    response.classList.remove("none")
+                    img_true.classList.remove("none")
                     img_true.classList.add("block")
                     let registro = new Date();
                     cliente.number_scanners++
                     fetch(`https://scanner-f97e1-default-rtdb.firebaseio.com/clientes/${index}.json`, {
                         method: "PATCH",
                         body: JSON.stringify({
-                            date_scanner: registro.getHours+ ":" + registro.getMinutes,
+                            date_scanner: registro.getHours.toLocaleString()+ ":" + registro.getHours.toLocaleString(),
                             number_scanners: cliente.number_scanners
                         }),
                         headers: {
@@ -37,17 +27,10 @@ scanner.addListener('scan', function(content) {
                     })
                     return
                 }else{
-                    if(cliente.number_scanners>=1) response.innerHTML="Codigo ya fue verificado-No valido"
+                    if(cliente.number_scanners>=1) response.innerHTML="Codigo ya fue verificado-No valido "+cliente.date_scanner
                     return
                 }
             }
         })
     });
-});
-Instascan.Camera.getCameras().then(cameras => {
-    if(cameras.length > 0){             
-        scanner.start(cameras[2]);
-    } else {
-        console.error("No existe câmera no dispositivo!");
-    }
 });
